7e3ad75194df18b38f2abc779633b9d9d56b4d1c
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-04-26 20:57:11 +0200

    Home View Controller

---
+++ netflix clone/netflix clone/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+        view.backgroundColor = .red
+        
+    }
+    
+
+  
+
+}
