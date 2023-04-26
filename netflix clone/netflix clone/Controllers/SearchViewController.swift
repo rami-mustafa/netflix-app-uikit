e9942563269c53a2131a61ee5a43823ac3723f75
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-04-26 20:58:00 +0200

    Search View Controller

---
+++ netflix clone/netflix clone/Controllers/SearchViewController.swift
@@ -0,0 +1,17 @@
+
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+    
+
+  
+
+}
