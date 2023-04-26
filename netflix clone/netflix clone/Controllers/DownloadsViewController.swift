e49b49cb2eb88efa617edff82105e55b07a221cc
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-04-26 20:56:47 +0200

    Up coming View Controller

---
+++ netflix clone/netflix clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+        
+    }
+    
+
+    
+}
