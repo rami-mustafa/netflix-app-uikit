f4f692b7bee5580ae220da2427f055054904ee24
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-04-26 20:57:41 +0200

    Up coming View Controller

---
+++ netflix clone/netflix clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,14 @@
+
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         view.backgroundColor = .green
+    }
+    
+
+}
