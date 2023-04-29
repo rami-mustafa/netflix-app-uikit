f634e64c5c09cdc6ace265708df86300665668b1
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-04-29 16:17:51 +0200

    preparing the appearance of the HomeViewController

--- netflix clone/netflix clone/Controllers/HomeViewController.swift
+++ netflix clone/netflix clone/Controllers/HomeViewController.swift
@@ -2,16 +2,47 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
+    
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
- 
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
         
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
 
-  
+}
 
+extension HomeViewController: UITableViewDelegate , UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "rami"
+        cell.backgroundColor = .red
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
