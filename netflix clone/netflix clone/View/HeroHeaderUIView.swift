3607112337b9c86b34aa0edbcf2c3a59551e60fe
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-05-06 16:48:33 +0200

    Hero Header View

--- netflix clone/netflix clone/View/HeroHeaderUIView.swift
+++ netflix clone/netflix clone/View/HeroHeaderUIView.swift
@@ -3,6 +3,46 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
 
+    
+    
+    private let heroImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+        
+    }()
+    
+    
+    private func addGradient() {
+        let gradientlayer = CAGradientLayer()
+        gradientlayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientlayer.frame = bounds
+        layer.addSublayer(gradientlayer)
+    }
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
   
    
 }
