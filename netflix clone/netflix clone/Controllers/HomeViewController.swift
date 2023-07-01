57c15b91a6404c9e06e70c8ed7b96620124b2133
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-01 22:58:59 +0200

    scroll ViewDidScroll

--- netflix clone/netflix clone/Controllers/HomeViewController.swift
+++ netflix clone/netflix clone/Controllers/HomeViewController.swift
@@ -5,7 +5,8 @@ class HomeViewController: UIViewController {
     
    
     
-    
+    let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top rated"]
+
     
     
     private let homeFeedTable: UITableView = {
@@ -53,7 +54,7 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate , UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20 
+        return sectionTitles.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
@@ -71,4 +72,15 @@ extension HomeViewController: UITableViewDelegate , UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+           return sectionTitles[section]
+       }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+          let defaultOffset = view.safeAreaInsets.top
+          let offset = scrollView.contentOffset.y + defaultOffset
+          
+          navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+      }
 }
