c9659d4748ed73478d2337fcf00eb78a5d5bffaf
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-10 21:16:10 +0200

    Trending Tv Response

---
+++ netflix clone/netflix clone/models/Tv.swift
@@ -0,0 +1,26 @@
+//
+//  Tv.swift
+//  netflix clone
+//
+//  Created by Ghaiath Alhereh on 09.07.23.
+//
+
+import Foundation
+
+struct TrendingTvResponse: Codable {
+    let results: [Tv]
+}
+
+struct Tv: Codable {
+   
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+    
+}
