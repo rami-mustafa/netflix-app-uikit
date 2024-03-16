c02b4654af8dca880b81f2ce68822c723e18a36b
Rami Mustafa <ramimustafa77@gmail.com>
2024-03-16 12:34:22 +0300

    Upcoming Screen.

---
+++ netflix clone/netflix clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  netflix clone
+//
+//  Created by Rami Mustafa on 15.03.24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
